280c86dc4b7d7a2cb78363ce38ed956c8c42f2d4
Sonualam <sonualam1599@gmail.com>
2023-11-15 20:41:01 +0530

    style: adjusted width of contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 function Contact() {
   return (
-    <div id="contact" className="max-w-[1040px m-auto md:pl-20 p-4 py-16 ">
+    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 ">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
