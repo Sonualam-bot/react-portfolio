98faab4f889211ec8364acd332bf9bf0c75eeb73
Sonualam <sonualam1599@gmail.com>
2023-11-17 20:37:27 +0530

    refactor

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,10 +8,11 @@ function ProjectCard({ item, frontend }) {
       <img
         src={img}
         alt="/img"
-        className="rounded-xl group-hover:opacity-10 w-full h-full transition ease-in-out duration-1000 "
+        className="rounded-xl 
+        transition ease-in-out duration-1000 group-hover:opacity-10 w-full h-full  "
       />
 
-      <div className="hidden transition ease-in-out duration-1000 group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  ">
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  ">
         <h3 className="text-2xl font-bold text-white tracking-wider text-center">
           {title}
         </h3>
