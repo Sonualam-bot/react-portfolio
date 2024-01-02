f398b50eb0402f748ff81841c54afc2b8a9c7c84
Sonualam <sonualam1599@gmail.com>
2024-01-02 23:41:53 +0530

    fix: bug contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = async () => {
+    // e.preventDefault();
+    // setFormData({
+    //   name: "",
+    //   phone: "",
+    //   email: "",
+    //   subject: "",
+    //   message: "",
+    // });
+  };
+
   return (
     <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 ">
       <h1 className="pt-4 text-4xl font-bold text-center text-[#001b5e]">
@@ -9,6 +38,7 @@ function Contact() {
         action="https://getform.io/f/c2cca6fe-6b25-423a-8360-3f2400dc1ff5"
         method="POST"
         encType="multipart/form-data"
+        onSubmit={handleOnSubmit}
       >
         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
           <div className="flex flex-col">
@@ -17,6 +47,8 @@ function Contact() {
               className="border-2 rounded-lg p-3 flex border-gray-300"
               type="text"
               name="name"
+              value={formData?.name}
+              onChange={handleInputChange}
               required
             />
           </div>
@@ -26,6 +58,8 @@ function Contact() {
               className="border-2 rounded-lg p-3 flex border-gray-300"
               type="number"
               name="phone"
+              value={formData?.phone}
+              onChange={handleInputChange}
             />
           </div>
         </div>
@@ -35,6 +69,8 @@ function Contact() {
             className="border-2 rounded-lg p-3 flex border-gray-300"
             type="email"
             name="email"
+            value={formData?.email}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -44,6 +80,8 @@ function Contact() {
             className="border-2 rounded-lg p-3 flex border-gray-300"
             type="text"
             name="subject"
+            value={formData?.subject}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -53,12 +91,17 @@ function Contact() {
             className="border-2 rounded-lg p-3 border-gray-300"
             type="text"
             name="message"
+            value={formData?.message}
+            onChange={handleInputChange}
             rows="10"
             required
           ></textarea>
         </div>
 
-        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+        <button
+          type="submit"
+          className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg"
+        >
           Send Message
         </button>
       </form>
